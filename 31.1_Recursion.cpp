d7e5e2f39b9ba2fab42d33f3c93386531b90410b
Madhur_Purohit <madhurpurohit1234@gmail.com>
2024-09-18 16:34:24 +0530

    Completed 51 Days of #190 Days DSA challenge

---
+++ 31.1_Recursion.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void recursion(int n)
+{
+    //* Base case.
+    if (n == 0)
+    {
+        cout << "Happy Birthday" << endl;
+        return;
+    }
+
+    cout << n << " days left for birthday" << endl;
+
+    recursion(--n);
+}
+
+int main()
+{
+    system("clear");
+    int n;
+    cout << "Enter any number: ";
+    cin >> n;
+
+    //* Iterative approach.
+    for (int i = n; i > 0; i--)
+    {
+        cout << i << " days left for birthday" << endl;
+    }
+    cout << "Happy Birthday" << endl;
+
+    //* Recursive approach.
+    recursion(n);
+
+    return 0;
+}
