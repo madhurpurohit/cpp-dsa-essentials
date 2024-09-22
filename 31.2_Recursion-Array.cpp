f816a538f892d368de4c4b51a9c36f6e194d5d6e
Madhur_Purohit <madhurpurohit1234@gmail.com>
2024-09-23 00:44:51 +0530

    Completed 56 Days of #190 Days DSA cahllenge

---
+++ 31.2_Recursion-Array.cpp
@@ -0,0 +1,66 @@
+//* Print array using recursion.
+#include <iostream>
+using namespace std;
+
+//? From index 0 to index last.
+void printArray0(int arr[], int index, int size)
+{
+    if (index == size)
+        return;
+
+    cout << arr[index] << " ";
+    printArray0(arr, index + 1, size);
+}
+
+//? From last index to 0 index.
+void reverseArray(int arr[], int index)
+{
+    if (index < 0)
+        return;
+    cout << arr[index] << " ";
+    reverseArray(arr, index - 1);
+}
+
+//? Reverse with index 0.
+void reverseArray0(int arr[], int index, int size)
+{
+    if (index == size)
+        return;
+
+    reverseArray0(arr, index + 1, size);
+    cout << arr[index] << " ";
+}
+
+//? Array from index 5 to 0 but print in forward manner.
+void printArray(int arr[], int index)
+{
+    if (index < 0)
+        return;
+
+    printArray(arr, index - 1);
+    cout << arr[index] << " ";
+}
+
+int main()
+{
+    system("clear");
+    int arr[5] = {10, 11, 12, 13, 14};
+
+    cout << "Array from index 0 to last index is: \n";
+    printArray0(arr, 0, 5);
+    cout << endl;
+
+    cout << "Array from last index to 0 is: \n";
+    printArray(arr, 4);
+    cout << endl;
+
+    cout << "Reversed Array from last index to 0 is: \n";
+    reverseArray(arr, 4);
+    cout << endl;
+
+    cout << "Reversed Array from index 0 to last index is: \n";
+    reverseArray0(arr, 0, 5);
+    cout << endl;
+
+    return 0;
+}
