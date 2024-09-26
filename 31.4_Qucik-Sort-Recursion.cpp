bc8c3e87a33db22a7b129bfa0093e28d1e791ad1
Madhur_Purohit <madhurpurohit1234@gmail.com>
2024-09-26 15:08:38 +0530

    Completed 60 Days of #190 Days DSA challenge

---
+++ 31.4_Qucik-Sort-Recursion.cpp
@@ -0,0 +1,52 @@
+//* Arrange array in increasing order using Merge Sort.
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int partitionArray(int arr[], int start, int end)
+{
+    int pos = start;
+    for (int i = start; i <= end; i++)
+    {
+        if (arr[i] <= arr[end])
+        {
+            swap(arr[i], arr[pos]);
+            pos++;
+        }
+    }
+    return pos - 1;
+}
+
+void quickSort(int arr[], int start, int end)
+{
+    if (start >= end)
+        return;
+
+    int pivot = partitionArray(arr, start, end);
+
+    // Left side portion of array.
+    quickSort(arr, start, pivot - 1);
+
+    // Right side portion of array.
+    quickSort(arr, pivot + 1, end);
+}
+
+int main()
+{
+    system("clear");
+    int arr[] = {10, 3, 4, 1, 5, 6, 3, 2, 11, 9};
+
+    cout << "Array before quick sort is:\n";
+    for (int i = 0; i < 10; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    quickSort(arr, 0, 9);
+
+    cout << "Array after quick sort is:\n";
+    for (int i = 0; i < 10; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
