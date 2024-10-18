f8fdf9bbabd871c3fb35e81f161fc9f154b54995
Madhur_Purohit <madhurpurohit1234@gmail.com>
2024-10-18 23:10:03 +0530

    Completed 77 Days of #190 Days of DSA challenge

---
+++ 32.8.2_File-Handling.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    system("clear");
+    int n;
+    cout << "Enter the size: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the element: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    ofstream fout;
+    fout.open("zoom.txt");
+    fout << "Original Vector\n";
+    for (int i = 0; i < n; i++)
+        fout << arr[i] << " ";
+
+    fout << "\nSorted data\n";
+
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n; i++)
+        fout << arr[i] << " ";
+
+    fout.close();
+
+    return 0;
+}
---
+++ 32.8.3_File-Handling.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    system("clear");
+    ofstream fout;
+    fout.open("z.txt");
+    fout << "Hello Everyone.\n";
+    fout << "How are you?\n";
+    fout << "How is your study?\n";
+    fout.close();
+
+    //* How to read each string that is written in above file.
+    ifstream fin;
+    fin.open("z.txt");
+
+    string line;
+    while (getline(fin, line)) // Here getline is a looping function where fin is the file & line is a string , & fin means to read whole line & copy it into line. & it will stop where our file is ended.
+    {
+        cout << line << endl;
+    }
+
+    fin.close();
+
+    return 0;
+}
